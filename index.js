95e6bca4f57cb5644fac7847e16c4d397b402406
caitlin <caitlin.wink@gmail.com>
2021-03-17 18:54:55 -0700

    commit tree 77781e79e8f7b1a9520bb02c771a65b2e23329f5

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(parameter){
+  let result
+  if ( parameter < 400 ) {
+    result = 'This one is on me!'
+  } else if ( parameter < 2500 && 1999 < parameter) {
+    result = 'I will gladly take your thirty bucks.'
+  } else  { 
+    result = 'No can do.'
+  };
+return result
+
+}
+function ternaryCheckCity(city){ 
+  city === 'NYC'
+  return (city === 'NYC' ? 'Ok, sounds good.' : 'No go.')
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(Tip){
+  switch (Tip) {
+    case 'generous': 
+      return 'Thank you so much.'
+  break;
+    case 'not as generous': 
+      return'Thank you.'
+  break;
+    default : 
+      return'Bye.'
+ 
+  }
 }
